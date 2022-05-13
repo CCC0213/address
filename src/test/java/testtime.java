ff0f566d7690be222b23d13956b8d498059a71ea
Your Name <you@example.com>
2022-05-13 11:25:26 +0800

    20220513-1

--- src/test/java/testtime.java
+++ src/test/java/testtime.java
@@ -5,18 +5,22 @@ public class testtime {
 
     public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat sdf =new SimpleDateFormat("yyyyMMddHHmmss");
 
         String str1="20220513100000";
         String str2="20220513103000";
 
         //System.out.print(str2.compareTo(str1));
+        Boolean bl = minuteComper(str1,str2);
+        System.out.print(bl);
+
+    }
+
+    public static Boolean minuteComper(String str1, String str2) throws ParseException {
+        SimpleDateFormat sdf =new SimpleDateFormat("yyyyMMddHHmmss");
         long time1 = sdf.parse(str1).getTime();
         long time2 = sdf.parse(str2).getTime();
-        System.out.println(time1);
-        System.out.println(time2);
-        System.out.print(time2>time1);
-
+        Boolean bl = time1>time2;
+        return bl;
     }
 }
 
